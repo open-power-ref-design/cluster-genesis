d07e1c4628ba23044837793caec9c9ad0fc740e1
Jay W. Carman <jwcarman@us.ibm.com>
2019-02-15 10:28:18 -0600

    refactor: Update Ubuntu installer image URLs
    Ubuntu 18.04 URLs (and checksums) have been updated to point to the
    18.04.1 images.

--- scripts/python/lib/genesis.py
+++ scripts/python/lib/genesis.py
@@ -286,9 +286,9 @@ def get_os_profile_pointers():
         "ubuntu-14.04-server-amd64": "ubuntu-14.04.5-server-amd64",
         "ubuntu-16.04-server-amd64": "ubuntu-16.04.5-server-amd64",
         "ubuntu-16.04-server-ppc64el": "ubuntu-16.04.5-server-ppc64el",
-        "ubuntu-18.04-live-server-amd64": "ubuntu-18.04.1-live-server-amd64",
-        "ubuntu-18.04-server-amd64": "ubuntu-18.04.1-server-amd64",
-        "ubuntu-18.04-server-ppc64el": "ubuntu-18.04.1-server-ppc64el"}
+        "ubuntu-18.04-live-server-amd64": "ubuntu-18.04.2-live-server-amd64",
+        "ubuntu-18.04-server-amd64": "ubuntu-18.04.2-server-amd64",
+        "ubuntu-18.04-server-ppc64el": "ubuntu-18.04.2-server-ppc64el"}
 
 
 def check_os_profile(profile):
