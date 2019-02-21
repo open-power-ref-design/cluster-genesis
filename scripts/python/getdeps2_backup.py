027b6e674019ba817addb23603b64d6aca2a6520
Travis Jones <jonesdt@us.ibm.com>
2019-02-21 12:11:49 -0600

    #Fix: Added pip functionality to getdeps2.py (package dependency tooling phase 2)

--- scripts/python/getdeps2_backup.py
+++ scripts/python/getdeps2_backup.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- import argparse
+import argparse
 import sys
 import time
 import os.path
@@ -23,96 +23,81 @@ import re
 import code
 import yaml
 
- import lib.logger as logger
+import lib.logger as logger
 from lib.utilities import Color
 import lib.genesis as gen
 
- def main():
+def main():
     log = logger.getlogger()
     log.debug('log this')
     dep_path = gen.get_dependencies_path()
 
-     pip_pre_files = [
+    pip_pre_files = [
                      'client_pip_pre_install.txt',
                      'dlipy3_pip_pre_install.txt',
                      'dlipy2_pip_pre_install.txt',
                      'dlinsights_pip_pre_install.txt',
                     ]
 
-     pip_post_files = [
+    pip_post_files = [
                      'client_pip_post_install.txt',
                      'dlipy3_pip_post_install.txt',
                      'dlipy2_pip_post_install.txt',
                      'dlinsights_pip_post_install.txt',
                     ]
 
-     conda_pre_files = [
+    conda_pre_files = [
                      'dlipy3_conda_pre_install.txt',
                      'dlipy2_conda_pre_install.txt',
                      'dlinsights_conda_pre_install.txt',
                        ]
 
-     conda_post_files = [
+    conda_post_files = [
                      'dlipy3_conda_post_install.txt',
                      'dlipy2_conda_post_install.txt',
                      'dlinsights_conda_post_install.txt',
                        ]
 
-     yum_pre_files = ['client_yum_pre_install.txt']
+    yum_pre_files = ['client_yum_pre_install.txt']
 
-     yum_post_files = ['client_yum_post_install.txt']
-
-     def file_check(pre_files):
-        for f in pre_files:
-            pre_file_path = os.path.join(dep_path,f)
-            my_file = os.path.isfile(pre_file_path)
-            if my_file:
-                pass
-            else:
-                menu = True
-                while menu == True:
-                    opt = input(f'INFO - Would you like to recover "{f}" ?\n 1 - Yes \n 2 - No \n')
-                    if opt == '1':
-                        print (f'\nINFO - Located new pre path for "{f}"\n')
-                        menu = False
-                    elif opt == '2':
-                        menu = False
-                        sys.exit()
-                    else:
-                        print('\nPlese select a valid option')
+    yum_post_files = ['client_yum_post_install.txt']
 
 
-             code.interact(banner='file_check', local=dict(globals(), **locals()))   
-
-     def format_pkg_name(pkg, pkg_type):
+    def format_pkg_name(pkg, pkg_type):
         if pkg_type == 'yum':
             pkg_items = pkg.split()
             pkg_repo = pkg.split()[2]
             pkg_fmt_name = (pkg_items[0].rsplit('.', 1)[0] + '-' +
                             pkg_items[1] + '.' + pkg_items[0].rsplit('.', 1)[1])
 
-         elif pkg_type == 'conda':
-            pkg_items = pkg.split() + ['pip']
-            pkg_repo = pkg_items[3].rsplit('/')[-1]
-            if pkg_repo == 'pip':
-                pkg_fmt_name = pkg_items[0] + '=' + pkg_items[1]
-            else:
-                pkg_fmt_name = (pkg_items[0] + '-' +
-                                pkg_items[1] + '-' + pkg_items[2] + '.tar.bz2')
+        elif pkg_type == 'conda':
+            pkg_fmt_name = pkg.rpartition('/')[-1]
+            if '/linux-ppc64le' in pkg:
+                pkg_repo = pkg[:pkg.find('/linux-ppc64le')]
+            elif '/linux-64' in pkg:
+                pkg_repo = pkg[:pkg.find('/linux-64')]
+            elif '/noarch' in pkg:
+                pkg_repo = pkg[:pkg.find('/noarch')]
+            elif 'file://' in pkg:
+                pkg_repo = '/file'
+            pkg_repo = pkg_repo.rpartition('/')[-1]
+
+            #pkg_items = pkg.split() + ['pip']
+            #pkg_repo = pkg_items[3].rsplit('/')[-1]
+            #if pkg_repo == 'pip':
+            #    pkg_fmt_name = pkg_items[0] + '=' + pkg_items[1]
+            #else:
+            #    pkg_fmt_name = (pkg_items[0] + '-' +
+            #                    pkg_items[1] + '-' + pkg_items[2] + '.tar.bz2')
             #code.interact(banner='format conda', local=dict(globals(), **locals()))
 
-         elif pkg_type == 'pip':
-            pkg_items = pkg.split()
-            pkg_repo = 'pip'
-            version = pkg_items[1].replace('(','')
-            version = version.replace(')','') 
-            pkg_fmt_name = pkg_items[0] + '=' + version
 
-             #code.interact(banner='pip', local=dict(globals(), **locals()))
+        elif pkg_type == 'pip':
+            pass
 
-         return pkg_fmt_name, pkg_repo
+        return pkg_fmt_name, pkg_repo
 
-     def write_merged_files(merged_sets, pkg_type):
+    def write_merged_files(merged_sets, pkg_type):
         if pkg_type == 'yum':
             for repo in merged_sets:
                 file_name = repo.replace('/', '')
@@ -123,24 +108,19 @@ import lib.genesis as gen
                     d = {file_name: sorted(merged_sets[repo])}
                     yaml.dump(d, f, indent=4, default_flow_style=False)
 
-         elif pkg_type == 'conda':
+        elif pkg_type == 'conda':
             for repo in merged_sets:
                 file_name = f'{pkg_type}-{repo}.yml'
                 file_path = os.path.join(dep_path, file_name)
+                #code.interact(banner='write conda', local=dict(globals(), **locals()))
                 with open(file_path, 'w') as f:
                     d = {file_name: sorted(list(merged_sets[repo]))}
                     yaml.dump(d, f, indent=4, default_flow_style=False)
 
-         elif pkg_type == 'pip':
-            for repo in merged_sets:
-                file_name = f'{pkg_type}.yml'
-                file_path = os.path.join(dep_path, file_name)
-                with open(file_path, 'w') as f:
-                    d = {file_name: sorted(merged_sets[repo])}
-                    yaml.dump(d, f, indent=4, default_flow_style=False)
+        elif pk_type == 'pip':
+            pass
 
-
-     def get_repo_list(pkgs, pkg_type):
+    def get_repo_list(pkgs, pkg_type):
         repo_list = []
         if pkg_type == 'yum':
             for pkg in pkgs:
@@ -149,36 +129,45 @@ import lib.genesis as gen
                 if repo not in repo_list:
                     repo_list.append(repo)
 
-         if pkg_type == 'conda':
+        if pkg_type == 'conda':
             for pkg in pkgs:
                 #code.interact(banner='get repo list', local=dict(globals(), **locals()))
-                pkg_items = pkg.split()  + ['pip']
-                repo = pkg_items[3].rsplit('/')[-1]
+                #pkg_items = pkg.split()  + ['pip']
+                #repo = pkg_items[3].rsplit('/')[-1]
+
+                #repo = pkg.rpartition('/')[0]  # strip filename
+                #repo = repo[2 + repo.find('//'):]  # strip leading 'http://' & similar
+                if '/linux-ppc64le' in pkg:
+                    repo = pkg[:pkg.find('/linux-ppc64le')]
+                elif '/linux-64' in pkg:
+                    repo = pkg[:pkg.find('/linux-64')]
+                else:
+                    repo = pkg[:pkg.find('/noarch')]
+                repo = repo.rpartition('/')[-1]
+
                 if repo not in repo_list:
                     repo_list.append(repo)
 
-         if pkg_type == 'pip':
+        if pkg_type == 'pip':
             for pkg in pkgs:
-                pkg_items = pkg.split()
-                repo = 'pip'
+                repo = 'pip-pkgs'
                 if repo not in repo_list:
                     repo_list.append(repo)
+        return repo_list
 
-         return repo_list
+    def merge_function(pre_files, post_files, pkg_type):
 
-     def merge_function(pre_files, post_files, pkg_type):
-
-         # generate pre paths
+        # generate pre paths
         pre_paths = []
         for file in pre_files:
             pre_paths.append(os.path.join(dep_path, file))
 
-         # Generate post paths
+        # Generate post paths
         post_paths = []
         for file in post_files:
             post_paths.append(os.path.join(dep_path, file))
 
-         # Loop through the files
+        # Loop through the files
         pkgs = {}  # # {file:{repo:{pre:[], post: []}
         for i, pre_file in enumerate(pre_paths):
             file_name = os.path.basename(pre_file)
@@ -191,13 +180,13 @@ import lib.genesis as gen
             except FileNotFoundError as exc:
                 print(f'File not found: {pre_file}. Err: {exc}')
 
-             try:
+            try:
                 with open(post_file, 'r') as f:
                     post_pkgs = f.read().splitlines()
             except FileNotFoundError as exc:
                 print(f'File not found: {post_file}. Err: {exc}')
 
-             # Get the repo list
+            # Get the repo list
             repo_list = get_repo_list(post_pkgs, pkg_type)
             #code.interact(banner='got repo list', local=dict(globals(), **locals()))
             for repo in repo_list:
@@ -211,50 +200,48 @@ import lib.genesis as gen
                     if pkg_repo == repo:
                         pkgs[file_key][repo]['pre'].append(pkg_fmt_name)
 
-                 #code.interact(banner='two', local=dict(globals(), **locals()))
+                #code.interact(banner='two', local=dict(globals(), **locals()))
                 for pkg in post_pkgs:
                     # Format the name
                     pkg_fmt_name, pkg_repo = format_pkg_name(pkg, pkg_type)
                     if pkg_repo == repo:
                         pkgs[file_key][repo]['post'].append(pkg_fmt_name)
 
-         code.interact(banner='pre diff', local=dict(globals(), **locals()))
+        #code.interact(banner='pre diff', local=dict(globals(), **locals()))
 
-         diff_sets = {}
+        diff_sets = {}
 
-         # Post - pre pkg sets. (may need adjustment for different repo type)
+        # Post - pre pkg sets. (may need adjustment for different repo type)
         for _file in pkgs:
+            #code.interact(banner='diff', local=dict(globals(), **locals()))
             diff_sets[_file] = {}
             for repo in pkgs[_file]:
-                code.interact(banner='diff loop', local=dict(globals(), **locals()))
+                #code.interact(banner='diff loop', local=dict(globals(), **locals()))
                 post_minus_pre = (set(pkgs[_file][repo]['post']) -
                                   set(pkgs[_file][repo]['pre']))
                 diff_sets[_file][repo] = post_minus_pre
 
-         #code.interact(banner='post diff', local=dict(globals(), **locals()))
+        #code.interact(banner='post diff', local=dict(globals(), **locals()))
 
-         # Merge by repository
+        # Merge by repository
         merged_sets = {}
 
-         for _file in diff_sets:
+        for _file in diff_sets:
             for repo in diff_sets[_file]:
                 #code.interact(banner='merge loop', local=dict(globals(), **locals()))
                 if repo not in merged_sets:
                     merged_sets[repo] = set()
                 merged_sets[repo] = merged_sets[repo] | diff_sets[_file][repo]
 
-         #code.interact(banner='post merge', local=dict(globals(), **locals()))
+        #code.interact(banner='post merge', local=dict(globals(), **locals()))
 
-         write_merged_files(merged_sets, pkg_type)
+        write_merged_files(merged_sets, pkg_type)
 
-     file_check(pip_pre_files)
-    file_check(pip_post_files)
     #merge_function(yum_pre_files, yum_post_files, 'yum')
-    #merge_function(conda_pre_files, conda_post_files, 'conda')
-    merge_function(pip_pre_files, pip_post_files, 'pip')
+    merge_function(conda_pre_files, conda_post_files, 'conda')
 
 
- ##pip
+##pip
 #
 #        elif function == 'pip':
 #            try:
@@ -419,12 +406,12 @@ import lib.genesis as gen
 #    package_merge()
 
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     """Simple python template
     """
 
-     logger.create('nolog', 'info')
+    logger.create('nolog', 'info')
     log = logger.getlogger()
 
-     main()
+    main()
     print("\nINFO - Process Completed\n")
