e71bb30b09eed1fd7089684af286122e0ff206f4
Jay W. Carman <jwcarman@us.ibm.com>
2019-04-26 06:46:23 -0500

    fix: Decrement BMC credential count
    When validating BMC IPMI/OpenBMC credentials a count is stored to keep
    track of the successful connections. When a new successful connection is
    validated the count is decremented by the _total_ number of BMCs instead
    of 1. This resulted in a binary pass/fail if _any_ or _no_ BMC
    connections were validated.

--- scripts/python/validate_cluster_hardware.py
+++ scripts/python/validate_cluster_hardware.py
@@ -307,7 +307,7 @@ class ValidateClusterHardware(object):
                             self.log.debug(f'Node {node} is powered {r}')
                             bmc_ai[node] = tuple(cred_list[j][:-1])
                             cred_list[j][3] -= 1
-                            left -= left
+                            left -= 1
                             print(f'\r{tot - left} of {tot} nodes communicating via IPMI',
                                   end='')
                             sys.stdout.flush()
